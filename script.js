555b1fcbfdd9d1055a56c5adb92d9b8fe42059ca
Raghav-dev <rhk4133@gmail.com>
2026-02-13 00:35:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const songs = [];
 
-/* Auto 50 Songs */
 for (let i = 1; i <= 50; i++) {
     songs.push({
         name: "RHK SONG " + i,
@@ -22,62 +21,65 @@ function enterApp(){
     document.getElementById("musicApp").style.display="block";
 }
 
-/* Load Songs */
 songs.forEach((song,index)=>{
     const card=document.createElement("div");
     card.className="song-card";
     card.innerHTML=`
-    <img src="${song.img}">
-    <h3>${song.name}</h3>
+        <img src="${song.img}">
+        <h3>${song.name}</h3>
     `;
     card.onclick=()=>playSong(index);
     songGrid.appendChild(card);
 });
 
 function playSong(index){
+    currentIndex=index;
+    const song=songs[index];
 
-    currentIndex = index;
-
-    audio.src = songs[index].file;
+    audio.src=song.file;
     audio.play();
 
-    nowTitle.innerText = songs[index].name;
+    nowTitle.innerText=song.name;
 
-    playerView.style.backgroundImage=`url('${songs[index].img}')`;
+    playerView.style.backgroundImage=`url('${song.img}')`;
     playerView.style.display="flex";
 
-    cd.style.backgroundImage=`url('${songs[index].img}')`;
+    cd.style.backgroundImage=`url('${song.img}')`;
     cd.classList.add("playing");
+
+    document.getElementById("playBtn").innerText="⏸";
 }
 
 function togglePlay(){
     if(audio.paused){
         audio.play();
         cd.classList.add("playing");
-    } else {
+        document.getElementById("playBtn").innerText="⏸";
+    }else{
         audio.pause();
         cd.classList.remove("playing");
+        document.getElementById("playBtn").innerText="▶";
     }
 }
 
 function nextSong(){
     currentIndex++;
-    if(currentIndex >= songs.length) currentIndex = 0;
+    if(currentIndex>=songs.length) currentIndex=0;
     playSong(currentIndex);
 }
 
 function prevSong(){
     currentIndex--;
-    if(currentIndex < 0) currentIndex = songs.length - 1;
+    if(currentIndex<0) currentIndex=songs.length-1;
     playSong(currentIndex);
 }
 
-function forward10(){
-    audio.currentTime += 10;
+function plus10(){
+    audio.currentTime+=10;
 }
 
-function backward10(){
-    audio.currentTime -= 10;
+function minus10(){
+    audio.currentTime-=10;
 }
 
 function goBack(){
@@ -92,3 +94,6 @@ function searchSong(){
         card.style.display=card.innerText.toLowerCase().includes(input)?"block":"none";
     });
 }
+
+/* Auto play next when song ends */
+audio.addEventListener("ended",nextSong);

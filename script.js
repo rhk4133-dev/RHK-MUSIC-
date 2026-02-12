d54ad01a13086e22ae8591a5b3444f065b21d85c
Raghav-dev <rhk4133@gmail.com>
2026-02-12 17:21:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,23 +7,8 @@ let songs = [
 ];
 
 let audio = new Audio();
-let currentSong = -1;
 
 function playSong(index) {
-    if (currentSong !== index) {
-        audio.src = songs[index];
-        currentSong = index;
-        audio.play();
-        document.getElementById("nowPlaying").innerText = "Now Playing: Song " + (index + 1);
-    } else {
-        togglePlay();
-    }
-}
-
-function togglePlay() {
-    if (audio.paused) {
-        audio.play();
-    } else {
-        audio.pause();
-    }
+    audio.src = songs[index];
+    audio.play();
 }
